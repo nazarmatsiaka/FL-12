277804bd4e0fb1b75537ef368323cb0df07e93cc
nazarmatsiaka <nazarchukk06@gmail.com>
2020-04-15 21:06:07 +0300

    fix

---
+++ FL12_Webinar_HW12/build/precache-manifest.25cf986bea2d4a3475d2f1c768d4918c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b451b430ed8ae00e4ac098f51228ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85cc36f4d96ece1203f7",
+    "url": "/static/css/main.2a69999e.chunk.css"
+  },
+  {
+    "revision": "893be625875057f95987",
+    "url": "/static/js/2.6c7ccbc9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6c7ccbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85cc36f4d96ece1203f7",
+    "url": "/static/js/main.c22a972b.chunk.js"
+  },
+  {
+    "revision": "96292a54911be24a091b",
+    "url": "/static/js/runtime-main.006f4e07.js"
+  }
+]);
---
+++ FL12_Webinar_HW8-9/homework/build/precache-manifest.8a5706e492e09fabd00a93cd1b5cc5b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c2335498270ddbfa8d040eb3ddd405",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8fcd723fef1bccdf6a1",
+    "url": "/static/css/main.95b4f1b8.chunk.css"
+  },
+  {
+    "revision": "38c2b623a3f9187edb4e",
+    "url": "/static/js/2.69b8bcd5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.69b8bcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fcd723fef1bccdf6a1",
+    "url": "/static/js/main.bad7e7d1.chunk.js"
+  },
+  {
+    "revision": "96292a54911be24a091b",
+    "url": "/static/js/runtime-main.006f4e07.js"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/delete.650cdc57.svg"
+  },
+  {
+    "revision": "60a471253b51c49c21fd27ed158c6c08",
+    "url": "/static/media/edit.60a47125.svg"
+  },
+  {
+    "revision": "e47bde860158c52514a1386d28cb64c9",
+    "url": "/static/media/search.e47bde86.svg"
+  }
+]);
